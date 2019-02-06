78534e874249780ce0d12bf69bb5c85d83ecc221
Sudarshan Suresh Srikant <sudarshan1994@gmail.com>
2019-02-06 11:12:11 -0400

    [master] Fixes code for Q2

--- A2/Q2/Account.java
+++ A2/Q2/Account.java
@@ -1,19 +1,21 @@
 public class Account
 {
 	protected float balance;
-
+	
+	public Account() {
+		balance = 0.0f;
+	}
+	
 	public float getBalance()
 	{
 		return balance;
 	}
-
-	public void credit(float amount)
-	{
+	
+	public void credit(float amount) {
 		balance += amount;
 	}
 
-	public void debit(float amount)
-	{
+	public void debit(float amount) {
 		balance -= amount;
 	}
 }
--- A2/Q2/USDollarAccount.java
+++ A2/Q2/USDollarAccount.java
@@ -1,14 +1,16 @@
 public class USDollarAccount extends Account
 {
 	static final float EXCHANGE_RATE = 0.75f;
-
-	public void credit(float amount)
-	{
-		balance += amount * EXCHANGE_RATE;
+	
+	public USDollarAccount() {
+		super();
+	}
+	
+	public void credit(float amount) {
+		super.credit(amount * EXCHANGE_RATE);
 	}
 
-	public void debit(float amount)
-	{
-		balance -= amount * EXCHANGE_RATE;
-	}
+	public void debit(float amount) {
+		super.debit(amount * EXCHANGE_RATE);
+	}	
 }
